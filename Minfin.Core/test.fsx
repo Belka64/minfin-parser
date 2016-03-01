06445d3fd096792c5972d35c73140eb2a2e695ec
vby <vby@vby.com>
2016-03-01 22:12:32 +0200

    interesting impl

--- Minfin.Core/test.fsx
+++ Minfin.Core/test.fsx
@@ -1,20 +1,27 @@
-﻿
+﻿#r @"D:\fsharp\FSharp.Data.dll"
+open FSharp.Data
+open System.Net
+open System
+open FSharp.Data.JsonExtensions
+
+let currencies = ["usd";"eur";"rub"]
+let actions = ["buy";"sell"]
+let cities = ["kiev";"vinnitsa";"dnepropetrovsk";"donetsk";"zhitomir";
+                "zaporozhye";"ivano-frankovsk";"kievobl";"kirovograd";"lugansk";"lutsk";
+                "lvov";"nikolaev";"odessa";"poltava";"rovno";"sumy";"ternopol";
+                "uzhgorod";"kharkov";"kherson";"khmelnitskiy";"cherkassy";"chernigov";"chernovtsy"]
+
+let BuildUrl currency action city =
+    sprintf "http://minfin.com.ua/currency/auction/%s/%s/%s/" currency action city 
+
+let GetAuctionUrls currencies actions cities = 
+    List.map (fun cur-> BuildUrl cur, cur) currencies 
+    |> (fun funcs -> List.collect (fun act -> List.map (fun (f, cur) -> f act, act, cur) funcs) actions ) 
+    |> (fun funcs -> List.collect (fun city -> List.map (fun (f, act, cur) -> f city, city, act, cur) funcs) cities ) 
 
 
 
-let t = System.DateTime.Parse "08:55"
-
-let MakeSum (data:string) = 
-            let processStr (d:string) =  
-                d.Substring(0, d.Length - 1)
-            int (processStr (data.Replace(" ", "")))
-
-MakeSum "28 000 $"
+let build =
+    List.reduce (fun acc el -> List.collect (fun c -> c) (List.map (fun t-> List.map (fun x-> sprintf "%s/%s" x t ) acc) el) ) [["http://minfin.com.ua/currency/auction/"]; currencies; actions; cities]
 
 
-let make (d:string) =
-    let p d =  
-        System.Decimal.Parse d
-    p d
-
-make "27,8"
