d04d941fd804158a26839ecec9e282ec83317872
vby <vby@nemetos.net>
2016-03-06 21:51:40 +0200

    added migrations files

---
+++ Minfin.Dal/Migrations/Configuration.cs
@@ -0,0 +1,29 @@
+namespace Minfin.Dal.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Minfin.Dal.MinfinDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            //AutomaticMigrationDataLossAllowed = true;
+        }
+
+        protected override void Seed(Minfin.Dal.MinfinDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
